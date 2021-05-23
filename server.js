d2701aac4a030d0d4089eb171902d1f4007f0648
Vladimir Maryanchuk <39910684+Hesowam@users.noreply.github.com>
2021-05-23 22:00:35 +0300

    fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const upload = require("./upload");
 const cors = require("cors");
 const fileUpload = require('express-fileupload');
+const path = require('path');
 
 const server = express();
 
@@ -14,9 +15,14 @@ server.use(fileUpload({
   createParentPath: true
 }));
 
+server.use(express.static(path.join(__dirname, 'data')));
 server.use(cors(corsOptions));
 
 server.post("/upload", upload);
+server.get("/get/:path", (res, req)=> {
+    res.path.slice(0,"http://45.141.76.252:8000/get/".length);
+
+})
 
 server.listen(8000, () => {
   console.log("Server started!");
